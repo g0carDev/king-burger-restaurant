0c899693fe4a1bfac97c1fca730c04778e33fa59
Jovan Gómez <111164889+g0carDev@users.noreply.github.com>
2024-01-29 15:06:16 -0600

    first commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+/*=============== HOME SWIPER ===============*/
+var homeSswiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
